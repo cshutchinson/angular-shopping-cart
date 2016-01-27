4d06305bf1cabeba405b4c8d940506bdc6925e0c
Christopher Hutchinson <cshutchinson@gmail.com>
2016-01-27 14:37:02 -0700

    bootstrapped project

---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('sc', ['ngRoute'])
+
+app.config(function($routeProvider) {
+    $routeProvider
+      .when('/store/', {
+        templateUrl: '/partials/store.html',
+        controller: 'StoreController'
+      })
+      .when('/cart/', {
+        templateUrl: '/partials/cart.html',
+        controller: 'CartController'
+      });
+});
---
+++ app/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('sc').controller("StoreController", function($scope){
+
+});
+
+angular.module('sc').controller("CartController", function($scope){
+  
+});
