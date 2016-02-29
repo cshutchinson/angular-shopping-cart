e410bbeb228d8acf3c1415410de69af9da8f6596
Christopher Hutchinson <cshutchinson@gmail.com>
2016-02-28 18:05:53 -0700

    angular shopping cart

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,12 @@ var app = angular.module('sc', ['ngRoute'])
 
 app.config(function($routeProvider) {
     $routeProvider
-      .when('/store/', {
-        templateUrl: '/partials/store.html',
+      .when('/store', {
+        templateUrl: 'partials/store.html',
         controller: 'StoreController'
       })
-      .when('/cart/', {
-        templateUrl: '/partials/cart.html',
+      .when('/cart', {
+        templateUrl: 'partials/cart.html',
         controller: 'CartController'
       });
 });
